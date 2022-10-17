8b6d9cc9aed220483e6fd1ef564c11f53ea62457
aqsafaisal34 <aqsay.faisal34@gmail.com>
2022-10-17 14:23:46 +0500

    new changes

--- server.mjs
+++ server.mjs
@@ -55,7 +55,7 @@ app.use(express.json()); // parsing body
 
 
 app.use(cors({
-    origin: ['http://localhost:3000', "*"],
+    origin: ['http://localhost:3000',https://super-cool-site-by-aqsay-faisal34-gmail-com-64ff6.netlify.app/, "*"],
     credentials: true
 }));
 
